cea3be72a3476b6e876fb0c5c48a7c604fc3610b
Ryan Thomas <rythomas2525@gmail.com>
2020-04-24 16:57:46 -0600

    signup page design almost done

--- client/src/pages/Landing/index.js
+++ client/src/pages/Landing/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from "../../components/Container"
 import Col from "../../components/Col"
 import Row from "../../components/Row"
-import backgroundImage from "../../images/1.jpg"
 import "./style.css"
 
 
@@ -16,7 +15,7 @@ function Landing() {
         <div className="background">
             <Container>
                 <Row>
-                    <Col size="md-4" style={{ backgroundColor: "rgba(127, 223, 255,0.6)", marginTop: "90px", height: "430px" }} >
+                    <Col size="md-4" style={{ backgroundColor: "rgba(127, 223, 255,0.6)", marginTop: "56px", height: "430px" }} >
                         <h2 className="text-center">Keep track of your spending with</h2>
                         <h1 className="text-center">TTF</h1>
                     </Col>
