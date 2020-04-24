458777b7379e3c8a52c7d85a6695b7b46805f087
Ryan Thomas <rythomas2525@gmail.com>
2020-04-23 20:10:48 -0600

    nav bar set up with bootstrap

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -3,46 +3,57 @@ import { Link } from 'react-router-dom'
 import * as ROUTES from '../../constants/routes';
 import SignOutButton from '../SignOut';
 import { AuthUserContext } from '../Session';
+import { Navbar, Nav } from 'react-bootstrap'
 
 
 const Navigation = () => (
-    <div>
-        <AuthUserContext.Consumer>
-            {authUser =>
-                authUser ? <NavigationAuth /> : <NavigationNonAuth />
-            }
-        </AuthUserContext.Consumer>
-    </div>
+
+
+    <Navbar bg="dark" variant="light">
+
+        <Nav className="mr-auto ">
+            <AuthUserContext.Consumer>
+                {authUser =>
+                    authUser ? <NavigationAuth /> : <NavigationNonAuth />
+                }
+            </AuthUserContext.Consumer>
+        </Nav>
+
+    </Navbar>
+
+
+
 );
 
 
 const NavigationAuth = () => (
-    <ul>
-        <li>
-            <Link to={ROUTES.LANDING}>Landing</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.HOME}>Home</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.ACCOUNT}>Account</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.ADMIN}>Admin</Link>
-        </li>
-        <li>
-            <SignOutButton />
-        </li>
-    </ul>
+    <div>
+
+        <Link className="navbar-brand" to={ROUTES.LANDING}>Landing</Link>
+
+
+        <Link className="navbar-brand" to={ROUTES.HOME}>Home</Link>
+
+        <Link className="navbar-brand" to={ROUTES.ACCOUNT}>Account</Link>
+
+        <Link className="navbar-brand" to={ROUTES.ADMIN}>Admin</Link>
+
+
+
+        <SignOutButton />
+
+
+    </div>
+
 );
 const NavigationNonAuth = () => (
-    <ul>
-        <li>
-            <Link to={ROUTES.LANDING}>Landing</Link>
-        </li>
-        <li>
-            <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-        </li>
-    </ul>
+
+    <div>
+        <Link className="navbar-brand" to={ROUTES.LANDING}>Landing</Link>
+
+
+        <Link className="navbar-brand" to={ROUTES.SIGN_IN}>Sign In</Link>
+    </div>
+
 );
 export default Navigation;
