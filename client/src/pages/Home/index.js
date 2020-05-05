7d0e8bdd07c570667b8ef81d210f6d069b9a55d5
Tyler Cain <tecain13@gmail.com>
2020-05-04 18:31:43 -0600

    commented out draft for date sort function;

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,18 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { AuthUserContext, withAuthorization } from '../../components/Session';
+import { withAuthorization } from '../../components/Session';
 import Container from "../../components/Container"
 import API from "../../util/API"
 import TransactionHist from "../../components/TransactionHist";
 import LineGraph from "../../components/LineGraph";
 import PieChart from "../../components/PieChart";
 import AddTransactionButton from "../../components/AddTransactionButton";
-import { Tabs, Tab, Row } from 'react-bootstrap/'
-import Col from "../../components/Col"
+import { Tabs, Tab, Row, Col } from 'react-bootstrap/'
 import "./Home.css"
 
 
 
-const HomePage = () => {
+function HomePage() {
 
     function addTransactionHandler(e) {
         console.log(e);
@@ -33,42 +32,36 @@ const HomePage = () => {
 
 
 
-    return <AuthUserContext.Consumer>
-        {authUser => (
-            <Row>
-                {/* <Col size="md-1"></Col> */}
-                <Col size="md-12">
-                    <Container className="dashboardBackground">
-
-                        <Tabs defaultActiveKey="table" id="uncontrolled-tab-example">
-                            <Tab eventKey="table" title="Transaction Details">
-                                <TransactionHist />
-                            </Tab>
-                            <Tab eventKey="piechart" title="Spending Categories">
-                                <PieChart />
-                            </Tab>
-                            <Tab eventKey="barchart" title="Balance History">
-                                <LineGraph />
-                            </Tab>
-                        </Tabs>
-
-                        <AddTransactionButton onClick={addTransactionHandler}>
-                        </AddTransactionButton>
-
-                    </Container >
-                </Col>
-                {/* <Col size="md-1"></Col> */}
-            </Row>
-        )}
-    </AuthUserContext.Consumer>
+    return (
 
 
 
+        <Container className="dashboardBackground">
+
+            <Tabs defaultActiveKey="table" id="uncontrolled-tab-example">
+                <Tab eventKey="table" title="Transaction Details">
+                    <TransactionHist transactions={transactions} />
+                </Tab>
+                <Tab eventKey="piechart" title="Spending Categories">
+                    <PieChart />
+                </Tab>
+                <Tab eventKey="barchart" title="Balance History">
+                    <LineGraph />
+                </Tab>
+            </Tabs>
+
+            <AddTransactionButton onClick={addTransactionHandler}>
+            </AddTransactionButton>
+
+        </Container >
 
 
+    );
 }
 
 const condition = authUser => !!authUser;
+
+
 
 export default withAuthorization(condition)(HomePage);
 
@@ -79,3 +72,4 @@ export default withAuthorization(condition)(HomePage);
 
 
 
+  
